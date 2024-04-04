8071b50c193ec9903f19c69f64aad8c2764c8a40
jaypavendano <jaypavendano@gmail.com>
2024-04-04 17:19:16 +0800

    initial commit

---
+++ app/components/banner.tsx
@@ -0,0 +1,18 @@
+export default function Banner() {
+  return (
+    <div className="bg-ellipse bg-no-repeat bg-center bg-auto">
+      <div className="container min-h-screen grid items-center content-center max-w-3xl gap-y-8">
+        <div>
+          <h3 className="text-2xl text-white">hey there 👋, my name is</h3>
+          <h1 className="font-space_Grotesk text-primary text-7xl font-bold mt-5 tracking-wide">
+            Jaypee Avendaño
+          </h1>
+        </div>
+        <p className="text-lg">
+          I'm a web developer committed to crafting seamless user experiences
+          and adept at creating API-driven products.
+        </p>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '20px',
+      screens: {
+        lg: '1440px',
+      },
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        ellipse: "url('/ellipse.png')",
+      },
+      colors: {
+        primary: '#00D8FF',
+        neutral_white: '#D4D4D4',
+        bg: '#111010',
+      },
+      fontFamily: {
+        // corsiva: ['MtCorsiva'],
+        space_Grotesk: ['var(--font-space_Grotesk)'],
       },
     },
   },
